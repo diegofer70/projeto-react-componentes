bbc25e6a258c4b578ad78d1cd2cd94b6b6b7dbec
Diego Araujo <ferreiradearaujod@gmail.com>
2024-07-17 23:01:07 -0300

    Adicionar arquivos componentes ProducttCard

---
+++ src/components/ProductCard.jsx
@@ -0,0 +1,10 @@
+export default function ProductCard() {
+    return (
+      <div className="product">
+        <img src="https://placehold.co/200x200" alt="Foto de tenis" />
+        <p>Título: Tênis Caro</p>
+        <p>Preço: R$ 20,00</p>
+        <p>Preço com desconto: R$ 10,00</p>
+      </div>
+    )
+  }
